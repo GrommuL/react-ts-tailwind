6caf2626b5f6968926a68c512643d9f8bb6c2a5c
GrommuL <miketsvg@gmail.com>
2023-03-29 18:28:31 +0600

    Добавил маршрутизацию

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,23 @@
+import About from '@/pages/About'
+import Contacts from '@/pages/Contacts'
+import Home from '@/pages/Home'
+import NotFound from '@/pages/NotFound'
+import Shop from '@/pages/Shop'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const Layout: React.FC = (): JSX.Element => {
-	return <></>
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/shop' element={<Shop />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/contacts' element={<Contacts />} />
+				<Route path='/*' element={<NotFound />} />
+			</Routes>
+		</>
+	)
 }
 
 export default Layout
