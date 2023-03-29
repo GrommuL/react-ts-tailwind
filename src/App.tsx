a9543d6f94a83695c3f3b0c9cfa3b730533e8358
GrommuL <miketsvg@gmail.com>
2023-03-29 17:40:44 +0600

    Добавил базовые стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App: React.FC = (): JSX.Element => {
-	return <></>
+	return <>asdasdsad SDSAD</>
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{html,css,scss,js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			container: {
+				padding: '15px',
+				center: true
+			},
+			colors: {
+				black: '#000000',
+				white: '#ffffff',
+				aqua: '#6E9C9F',
+				aquaDark: 'rgba(110, 156, 159, 0.1)',
+				grey: '#D5D5D5',
+				greyLight: '#D1D1D1',
+				linen: '#F1EADC',
+				gold: '#998E78',
+				pinkDark: '#927876',
+				pink: '#FD9696',
+				yellow: '#FDC796'
+			}
+		}
 	},
 	plugins: []
 }
